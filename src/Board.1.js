39c704110db36745cd86e8700c19898c454f3f92
userdefault13 <user.default@me.com>
2023-07-08 19:44:20 -0700

    fixed row 1

--- src/Board.1.js
+++ src/Board.1.js
@@ -1,8 +1,6 @@
 import Phaser from "phaser";
 import CornerTile from "./tiles/CornerTile";
 import PropertyTile from './tiles/PropertyTile.js';
-import ChanceTile from "./tiles/ChanceTile";
-import CommunityChestTile from "./tiles/CommunityChestTile";
 
 export class Board extends Phaser.Scene {
     constructor() {
@@ -24,16 +22,33 @@ export class Board extends Phaser.Scene {
         const goCornerRectangle = this.add.rectangle();
         const goCorner = new CornerTile(0);
         goCorner.render(goCornerRectangle, currentX, currentY);
-
+        currentX += goCorner.width-30; // Move to the next tile position
         
         // Draw the property tiles
         for (var i = 1; i <= numProperties; i++) {
-            currentX += goCorner.width; // Move to the next tile position
-            const propertyRectangle = this.add.rectangle();
-            const propertyTile = new PropertyTile(`Property ${i}`, 200, [20, 100, 300, 750, 925, 1100], 150, 150, i);
-            propertyTile.render(propertyRectangle, currentX, currentY);
-            currentX += propertyTile.width; // Update the currentX position for the next tile
+            const propertyVerticalRectangle = this.add.rectangle();
+            const propertyVerticalTile = new PropertyTile(`Property ${i}`, 200, [20, 100, 300, 750, 925, 1100], 150, 150, i);
+            propertyVerticalTile.render(propertyVerticalRectangle, currentX, currentY);
+            currentX += propertyVerticalTile.width; // Update the currentX position for the next tile
         }
+
+        // Draw the jail property
+        const jailCornerRectangle = this.add.rectangle();
+        const jailCorner = new CornerTile(0);
+        jailCorner.render(jailCornerRectangle, currentX, currentY);
+        currentY = 600;
+
+        // Draw the property tiles
+        for (var i = 1; i <= numProperties; i++) {
+            const propertyHorizontalRectangle = this.add.rectangle();
+            const propertyHorizontalTile = new PropertyTile(`Property ${i}`, 200, [20, 100, 300, 750, 925, 1100], 150, 150, i);
+            // interchange width and height
+            propertyHorizontalTile.width = 240;
+            propertyHorizontalTile.height = 180;
+            propertyHorizontalTile.render(propertyHorizontalRectangle, currentX, currentY);
+            currentY = 240+20; // Update the currentX position for the next tile
+        }
+        
         
     }
 }
